73594b7e83ce9eb418dd279315f8b76ec4e7b05a
Domenico Manna <mannadomenico2849@gmail.com>
2019-04-23 17:52:32 -0400

    Retrieve data from Github API

---
+++ js/githubClient.js
@@ -0,0 +1,24 @@
+import {config} from './config.js'
+
+export default function GithubClient(){
+
+}
+
+GithubClient.prototype.getUser = async function(user){
+    let userUrl = `https://api.github.com/users/${user}?client_id=${config.githubClientId}&client_secret=${config.githubSecretId}`
+
+    return await getData(userUrl);
+}
+
+GithubClient.prototype.getRepositories = async function(user){
+    let repositoriesUrl = `https://api.github.com/search/repositories?q=user:dom2849&sort=updated&order=desc?client_id=${config.githubClientId}&client_secret=${config.githubSecretId}`
+
+    return await getData(repositoriesUrl);
+}
+
+async function getData(url){
+    let response = await fetch(url);
+    if (!response.ok) throw new Error("URL not found");
+    
+    return await response.json();
+}
