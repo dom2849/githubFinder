4b3307b8ffd3249088169dacad201ef4b0704e00
Domenico Manna <mannadomenico2849@gmail.com>
2019-04-23 19:39:34 -0400

    Refactor code

--- js/githubClient.js
+++ js/githubClient.js
@@ -1,24 +1,25 @@
-import {config} from './config.js'
+import { config } from './config.js'
 
-export default function GithubClient(){
+export default function GithubClient() {
 
 }
 
-GithubClient.prototype.getUser = async function(user){
+
+GithubClient.prototype.getUser = async function (user) {
     let userUrl = `https://api.github.com/users/${user}?client_id=${config.githubClientId}&client_secret=${config.githubSecretId}`
 
-    return await getData(userUrl);
+    return getData(userUrl);
 }
 
-GithubClient.prototype.getRepositories = async function(user){
+GithubClient.prototype.getRepositories = function (user) {
     let repositoriesUrl = `https://api.github.com/search/repositories?q=user:dom2849&sort=updated&order=desc?client_id=${config.githubClientId}&client_secret=${config.githubSecretId}`
 
-    return await getData(repositoriesUrl);
+    return getData(repositoriesUrl);
 }
 
-async function getData(url){
+async function getData(url) {
     let response = await fetch(url);
     if (!response.ok) throw new Error("URL not found");
-    
+
     return await response.json();
 }
